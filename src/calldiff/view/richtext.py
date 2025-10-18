2433d6e8fefb396fc36f5fd9d573cb0096c7ffcf
Jim King <greg.luterman@synapse-wireless.com>
2025-10-17 22:30:32 -0500

    Very basic text styling

--- src/calldiff/view/richtext.py
+++ src/calldiff/view/richtext.py
@@ -22,10 +22,23 @@ class TextChunk:
 
 class RichText(richtext.RichTextCtrl):
     chunks: List[TextChunk]
+    length: int = 0
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.chunks = []
 
     def add_chunk(self, text_type: Actions, text: str):
+        if text_type == Actions.EXPOSITION:
+            style = wx.TextAttr(wx.RED)
+        else:
+            style = wx.TextAttr(wx.BLACK)
         self.chunks.append(TextChunk(text_type, text))
         self.AppendText(text)
+        new_len = self.length + len(text)
+        self.SetStyle(self.length, new_len, style)
+        self.length = new_len
+
+    def Clear(self):
+        self.chunks = []
+        self.length = 0
+        super().Clear()
